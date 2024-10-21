11731e6a2fb62c896fe42e9c9f6ec434ef63372f
Alia Brahmi <146718686+AliaBrah@users.noreply.github.com>
2024-10-21 11:09:42 +0200

    6 événements dans la base

--- Data_base_donnee/Base_donnee/Script_cycle_manifestations.sql
+++ Data_base_donnee/Base_donnee/Script_cycle_manifestations.sql
@@ -16,3 +16,41 @@ ON m.fk_personne = p.pk_personne
 JOIN organisation_association oa 
 ON m.fk_organisation = oa.pk_organisation 
 WHERE m.pk_membre = 1 AND p2.pk_participation = 1;
+
+
+SELECT e.nom ,l.ville , s."type" 
+FROM "source" s 
+JOIN liaision_source_evenement lse 
+ON s.pk_source = lse.fk_source 
+JOIN evenement e 
+ON e.pk_evenement = lse.fk_evenement
+JOIN lieu l 
+ON l.pk_lieu = e.fk_lieu_debut ;
+
+SELECT e.nom , e.date , l.ville , COUNT(DISTINCT s."type") AS types_differents_sources
+FROM "source" s 
+JOIN liaision_source_evenement lse 
+ON s.pk_source = lse.fk_source 
+JOIN evenement e 
+ON e.pk_evenement = lse.fk_evenement
+JOIN lieu l 
+ON l.pk_lieu = e.fk_lieu_debut
+GROUP BY e.pk_evenement ;
+
+SELECT
+    e.nom AS evenement,
+    e.date ,
+    l.ville,
+    s."type",
+    COUNT(s.pk_source) AS nombre_objets_lies
+FROM "source" s 
+JOIN liaision_source_evenement lse 
+ON s.pk_source = lse.fk_source 
+JOIN evenement e 
+ON e.pk_evenement = lse.fk_evenement
+JOIN lieu l 
+ON l.pk_lieu = e.fk_lieu_debut
+GROUP BY
+    e.pk_evenement , s."type" 
+ORDER BY
+    e.pk_evenement , s."type" ;
